60ee8e5186d80010fe7ba814b7d882a6b0076ec8
yx <130053608+yixin16@users.noreply.github.com>
2025-12-13 20:36:46 +0800

    LC-3sum [Runtime: 351 ms; Memory: 20.8 MB]

--- array/3sum.py
+++ array/3sum.py
@@ -11,22 +11,25 @@ class Solution:
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            target = 0 - nums[i]
             start, stop = i + 1, n - 1
+            target = -nums[i]
+
             while start < stop:
                 total = nums[start] + nums[stop]
 
                 if total < target:
                     start += 1
-
                 elif total > target:
                     stop -= 1
-
                 else:
                     result.append([nums[i], nums[start], nums[stop]])
 
                     start += 1
+                    stop -= 1
+
                     while start < stop and nums[start] == nums[start - 1]:
                         start += 1
+                    while start < stop and nums[stop] == nums[stop + 1]:
+                        stop -= 1
 
         return result
