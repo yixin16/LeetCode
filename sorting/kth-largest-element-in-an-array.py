df02e228cd93a5b3173aafc4980446531ed54870
yx <130053608+yixin16@users.noreply.github.com>
2025-12-15 14:20:40 +0800

    LC-kth-largest-element-in-an-array [Runtime: N/A; Memory: N/A]

--- sorting/kth-largest-element-in-an-array.py
+++ sorting/kth-largest-element-in-an-array.py
@@ -1,12 +1,29 @@
-import heapq
+import random
 
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        min_heap = nums[:k]
-        heapq.heapify(min_heap)
+    def findKthLargest(self, nums, k):
+        k = k - 1  # 第 k 大 → index k-1
 
-        for num in nums[k:]:
-            if num > min_heap[0]:
-                heapq.heappushpop(min_heap, num) 
-        
-        return min_heap[0]
+        def quickselect(left, right):
+            pivot_idx = random.randint(left, right)
+            pivot = nums[pivot_idx]
+
+            # 把 pivot 移到末尾
+            nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
+
+            store_idx = left
+            for i in range(left, right):
+                if nums[i] > pivot: 
+                    nums[i], nums[store_idx] = nums[store_idx], nums[i]
+                    store_idx += 1
+
+            nums[store_idx], nums[right] = nums[right], nums[store_idx]
+
+            if store_idx == k:
+                return nums[store_idx]
+            elif store_idx > k:
+                return quickselect(left, store_idx - 1)
+            else:
+                return quickselect(store_idx + 1, right)
+
+        return quickselect(0, len(nums) - 1)
