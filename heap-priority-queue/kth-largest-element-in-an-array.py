7cb830ee727c6c3a46281fb2b21091f30be1ba4e
yx <130053608+yixin16@users.noreply.github.com>
2025-12-15 14:24:22 +0800

    LC-kth-largest-element-in-an-array [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/kth-largest-element-in-an-array.py
+++ heap-priority-queue/kth-largest-element-in-an-array.py
@@ -2,28 +2,25 @@ import random
 
 class Solution:
     def findKthLargest(self, nums, k):
-        k = k - 1  # 第 k 大 → index k-1
+        k -= 1
+        left, right = 0, len(nums) - 1
 
-        def quickselect(left, right):
+        while True:
             pivot_idx = random.randint(left, right)
             pivot = nums[pivot_idx]
-
-            # 把 pivot 移到末尾
             nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
 
-            store_idx = left
+            store = left
             for i in range(left, right):
-                if nums[i] > pivot: 
-                    nums[i], nums[store_idx] = nums[store_idx], nums[i]
-                    store_idx += 1
+                if nums[i] > pivot:
+                    nums[i], nums[store] = nums[store], nums[i]
+                    store += 1
 
-            nums[store_idx], nums[right] = nums[right], nums[store_idx]
+            nums[store], nums[right] = nums[right], nums[store]
 
-            if store_idx == k:
-                return nums[store_idx]
-            elif store_idx > k:
-                return quickselect(left, store_idx - 1)
+            if store == k:
+                return nums[store]
+            elif store < k:
+                left = store + 1
             else:
-                return quickselect(store_idx + 1, right)
-
-        return quickselect(0, len(nums) - 1)
+                right = store - 1
