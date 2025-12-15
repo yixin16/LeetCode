a2f01b762f51155182daa41cc57555bc6c8a9106
yx <130053608+yixin16@users.noreply.github.com>
2025-12-15 14:25:01 +0800

    LC-kth-largest-element-in-an-array [Runtime: 99 ms; Memory: 28.8 MB]

--- heap-priority-queue/kth-largest-element-in-an-array.py
+++ heap-priority-queue/kth-largest-element-in-an-array.py
@@ -1,26 +1,10 @@
-import random
+import heapq
 
 class Solution:
     def findKthLargest(self, nums, k):
-        k -= 1
-        left, right = 0, len(nums) - 1
-
-        while True:
-            pivot_idx = random.randint(left, right)
-            pivot = nums[pivot_idx]
-            nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
-
-            store = left
-            for i in range(left, right):
-                if nums[i] > pivot:
-                    nums[i], nums[store] = nums[store], nums[i]
-                    store += 1
-
-            nums[store], nums[right] = nums[right], nums[store]
-
-            if store == k:
-                return nums[store]
-            elif store < k:
-                left = store + 1
-            else:
-                right = store - 1
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return heap[0]
