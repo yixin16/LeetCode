aca655020bc2d63c978ccb299526aa5a062b5c13
yx <130053608+yixin16@users.noreply.github.com>
2025-12-16 10:06:12 +0800

    LC-find-peak-element [Runtime: 0 ms; Memory: 18 MB]

--- binary-search/find-peak-element.py
+++ binary-search/find-peak-element.py
@@ -1,4 +1,11 @@
 class Solution:
     def findPeakElement(self, nums):
-        pos = nums.index(max(nums))
-        return pos
+        left, right = 0, len(nums) - 1
+        while left < right:
+            middle = (left + right) // 2
+            if nums[middle] < nums[middle+1]:
+                left = middle +1
+            else:
+                right = middle
+
+        return left
