2dda1a4ba0b2cb59ff883a574f1d37be32d4ba85
yx <130053608+yixin16@users.noreply.github.com>
2025-12-19 12:47:15 +0800

    LC-valid-parentheses [Runtime: 0 ms; Memory: 17.7 MB]

---
+++ stack/valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+
+        for ch in s:
+            if ch in mapping:
+                if not stack or stack[-1] != mapping[ch]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(ch)
+
+        return not stack
